fa0f927c6fadb431f252b60ea80a92eaf4e5ddc3
maowarisa <maowarisa123@gmail.com>
2024-05-29 12:11:58 +1000

    generate ID fucntion bug fixed

--- src/app/modules/users/user.utils.ts
+++ src/app/modules/users/user.utils.ts
@@ -1,23 +1,30 @@
 import { TAcademicSemester } from '../academicSemester/academicSemester.interface';
 import { Student } from '../student/student.model';
 import { User } from './user.model';
-const lastStudentId = async() =>{
-  const lastStudent = await User.findOne({
-    role: 'student'
-  },{
-    id: 1,
-    _id: 0
-  })
-  .sort({
-    createdAt : -1
-  })
-  .lean();
-  return Number(lastStudent?.id ? lastStudent.id.substring(6) : 0)
-}
-export const generatedStudentId = async(payload: TAcademicSemester) => {
-  console.log(await lastStudentId())
-  const currentId = await lastStudentId() || 0;
-  let incrementId = (currentId + 1).toString().padStart(4, '0');
+const findLastStudentId = async (searchExistingId: string) => {
+
+  const lastStudent = await User.findOne(
+    {
+      role: 'student',
+      id: { $regex: `^${searchExistingId}` },
+    },
+    {
+      id: 1,
+      _id: 0,
+    },
+  );
+  return lastStudent?.id ? lastStudent.id : undefined;
+};
+export const generatedStudentId = async (payload: TAcademicSemester) => {
+  let currentId = (0).toString(); // by default
+  const searchExistingId = payload.year + payload.code;
+
+  const lastStudentId = await findLastStudentId(searchExistingId);
+
+  if (lastStudentId) {
+    currentId = lastStudentId.substring(6);
+  }
+  let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
 
   incrementId = `${payload.year}${payload.code}${incrementId}`;
   return incrementId;
